0769c2e153266f3a344fe5ad22c8ee7947f17a50
nishant3180 <nishant1malhotra@gmail.com>
2018-06-04 16:22:22 +0530

    commit2 webscrapper change 12 file added

--- webScrapper.js
+++ webScrapper.js
@@ -7,11 +7,11 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(req, response) {
   console.log("hello hi hello hi");
-	request('http://www.mycashback.in/', function(err, resp, html) {
+	request('http://www.bbc.com/news', function(err, resp, html) {
         if (!err){
           const $ = cheerio.load(html);
           console.log(html); 
-		  $('div[id=popup1]').html()
+		  $('div[class=gel-layout__item gs-u-float-left@l gs-u-display-block@m gs-u-display-none@xs]').html()
 		  
       }
 	});
