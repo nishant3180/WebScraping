ad55bad51bf60f1fc4b596fea5128030f85bc9bf
nishant3180 <nishant1malhotra@gmail.com>
2018-06-04 15:53:08 +0530

    commit2 webscrapper change 7 file added

--- webScrapper.js
+++ webScrapper.js
@@ -7,6 +7,12 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
   console.log("hello hi hello hi");
+	request('http://www.google.com/', function(err, resp, html) {
+        if (!err){
+          const $ = cheerio.load(html);
+          console.log(html); 
+      }
+	});
 });
 
 app.listen(app.get('port'), function() {
