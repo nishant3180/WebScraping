ef955690e3b3cbed569361358509c8e0b0ed6aad
nishant3180 <nishant1malhotra@gmail.com>
2018-06-04 15:16:43 +0530

    commit2 webscrapper file added

--- webScrapper.js
+++ webScrapper.js
@@ -5,10 +5,12 @@ var app = express();
 
 
 app.get('/', function(request, response) {
-	request('http://www.google.com/', function(err, resp, html) {
+	
+	console.log('hello hi hello hi');
+	/*request('http://www.google.com/', function(err, resp, html) {
         if (!err){
           const $ = cheerio.load(html);
           console.log(html); 
       }
-	});
+	});*/
 )};
