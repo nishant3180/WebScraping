fd5991d199cbb743536b45b8bed687da7eb3e2b0
nishant3180 <nishant1malhotra@gmail.com>
2018-06-04 16:50:29 +0530

    commit2 webscrapper change 19 file added

--- webScrapper.js
+++ webScrapper.js
@@ -11,7 +11,7 @@ app.get('/', function(req, response) {
         if (!err){
           const $ = cheerio.load(html);
          // console.log(html); 
-		 var outputmy = $("div[class='nw-c-most-read__items gel-layout gel-layout--no-flex']").html();
+		 var outputmy = $("div[class='gel-layout__item gs-u-float-left@l gs-u-display-block@m gs-u-display-none@xs']").html();
 		  console.log('my output is');
 		  console.log(outputmy);
 		  
